d4d20f4d89ae476a003b4679a6cba37f2a7f437c
diahputr <46917636+diahputr@users.noreply.github.com>
2020-04-03 19:06:10 +0700

    Update index.php

--- get/index.php
+++ get/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>GET and POST Method</title>
+    <title>GET Method</title>
     <link rel="stylesheet" href="css/style.css"/>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/metode.js"></script>
