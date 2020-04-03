eb7d02b46f11de8a5c6faa78e8c205b0a70a84ff
diahputr <46917636+diahputr@users.noreply.github.com>
2020-04-03 20:20:02 +0700

    Update proses.php

--- get/proses.php
+++ get/proses.php
@@ -6,6 +6,6 @@ if(isset($_GET['fnama']))
  $lalamat = $_GET['lalamat'];
  $talamat = $_GET['talamat'];
  echo "<span class='success'><b>Sukses</b></span><br/>";
- echo "Nama : ".$fnama."<br/>Alamat Penjemputan : ".$lalamat"<br/>Alamat Tujuan: ".$talamat"<br/>Mohon bersiap, Driver menuju ke alamat penjemputan.";
+ echo "Nama : ".$fnama."<br/>Alamat Penjemputan : ".$lalamat"<br/>Alamat Tujuan: ".$talamat;
 }
 ?>
