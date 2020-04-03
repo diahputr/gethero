4bd6e77af3b34cfddffe96430d23f3928f106dc3
diahputr <46917636+diahputr@users.noreply.github.com>
2020-04-03 20:32:19 +0700

    Update metode.js

--- get/metode.js
+++ get/metode.js
@@ -5,7 +5,7 @@ $("#submit").click(function(){
 	var lalamat = $("#lalamat").val();
 	var talamat = $("#talamat").val();
 	
-	if( fnama != '' || lalamat !=''){
+	if( fnama != '' || lalamat !='' || talamat !='' ){
 		return true; 
 	}	
 	else{
