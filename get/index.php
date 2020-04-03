1c28801f46555fe83142f9804dca9798a86e5c75
diahputr <46917636+diahputr@users.noreply.github.com>
2020-04-03 20:10:57 +0700

    Update index.php

--- get/index.php
+++ get/index.php
@@ -11,13 +11,16 @@
 	<div class="container">
 		<div class="main">
 	      <form  method="get" action="index.php" id="form">
-			<h2>Form</h2><hr/>		
+			<h2>Form Pemesanan GoBluebird</h2><hr/>		
 			
 			<label>Nama :</label>
 			<input type="text" name="fnama" id="fnama" />
 			
-			<label>Alamat :</label>
-			<input type="text" name="lalamat" id="lalamat" />	
+			<label>Alamat Penjemputan:</label>
+			<input type="text" name="lalamat" id="lalamat" />
+		      	
+		      	<label>Alamat Tujuan:</label>
+			<input type="text" name="talamat" id="talamat" />
 			
 			<input type="submit" name="submit" id="submit" value="Submit">
 		  </for>
