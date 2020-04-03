ed06e5e49ad472e39eca56f1d3ecfbd60a570bdc
diahputr <46917636+diahputr@users.noreply.github.com>
2020-04-03 19:08:52 +0700

    Update proses.php

--- get/proses.php
+++ get/proses.php
@@ -1,17 +1,10 @@
 <?php 
-if(isset($_POST['fnama']))
-{
- $fnama = $_POST['fnama'];
- $lalamat = $_POST['lalamat'];
- echo "<span class='success'>Dengan <b>METODE POST</b></span><br/>";
- echo "Nama : ".$fnama."<br/>Alamat : ".$lalamat;
-}
 //--------------------------------------------------------------------
 if(isset($_GET['fnama']))
 {
  $fnama = $_GET['fnama'];
  $lalamat = $_GET['lalamat'];
- echo "<span class='success'>Dengan <b>METODE GET</b></span><br/>";
+ echo "<span class='success'><b>Sukses</b></span><br/>";
  echo "Nama : ".$fnama."<br/>Alamat : ".$lalamat;
 }
 ?>
